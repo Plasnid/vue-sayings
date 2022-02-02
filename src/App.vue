d6aff1b24c942b5cc72a0b59ba998f442cc2205f
Plasnid <stuart@sackler.ca>
2022-02-02 12:35:37 -0500

    initial files

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <SayingsDisplay :saying=this.sayings[this.activeSayingNum] @next="increaseSayingNum" @previous="decreaseSayingNum"/>
+  </div>
+</template>
+
+<script>
+import SayingsDisplay from './components/SayingsDisplay'
+
+export default {
+  name: 'App',
+  components: {
+    SayingsDisplay
+  },
+  data() {
+    return {
+      sayings: [
+        "I likes me a sammich",
+        "Hey is that a tentacle",
+        "Cheese is delicious",
+        "Dude, wheres my car?"
+        ],
+      activeSayingNum: 0
+    }
+  },
+  methods: {
+    increaseSayingNum() {
+      this.activeSayingNum +=1;
+      if(this.activeSayingNum>=this.sayings.length){
+        this.activeSayingNum = 0;
+      }
+      console.log(this.activeSayingNum);
+    },
+    decreaseSayingNum() {
+      this.activeSayingNum -=1;
+      if(this.activeSayingNum<0){
+        this.activeSayingNum = this.sayings.length-1;
+      }
+      console.log(this.activeSayingNum);
+    },
+    ticketyBoo(){
+      console.log("ticketyBoo");
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SayingsDisplay.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="sayingsDisplay">
+        <button @click="sayClicked">ticketyboo</button>
+        <button @click="tickDown">prev saying</button>
+        <button @click="tickUp">next saying</button>
+        {{this.saying}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SayingsDisplay',
+    props: {
+        saying: String
+    },
+    methods: {
+        sayClicked() {
+            console.log("I've been clicked!!!");
+        },
+        tickUp(){
+            console.log("say the next thing");
+            this.$emit("next");
+        },
+        tickDown(){
+            console.log("say the previous thing");
+            this.$emit("previous");
+        }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
